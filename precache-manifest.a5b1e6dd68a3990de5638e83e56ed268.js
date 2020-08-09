706af77c006392fdab961e2eef7611f180af4453
Ashleigh <ashleighc207@gmail.com>
2020-08-08 20:50:30 -0400

    Updates

---
+++ precache-manifest.a5b1e6dd68a3990de5638e83e56ed268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcc222dbbff0ab9acbd82ce1454bf82",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6f0331b9251bcd393868",
+    "url": "/portfolio/static/css/main.de50313e.chunk.css"
+  },
+  {
+    "revision": "461ffa2485bedd0d6dd5",
+    "url": "/portfolio/static/js/2.0cf6c287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.0cf6c287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0331b9251bcd393868",
+    "url": "/portfolio/static/js/main.102cf8c5.chunk.js"
+  },
+  {
+    "revision": "8ce6671c13d1c2b352ae",
+    "url": "/portfolio/static/js/runtime-main.2e09e129.js"
+  },
+  {
+    "revision": "674d59c792e9fc90021f7eed7a9f39b2",
+    "url": "/portfolio/static/media/color_palette.674d59c7.jpg"
+  },
+  {
+    "revision": "aad5454dcfb3f53eac442ad196d75eab",
+    "url": "/portfolio/static/media/logo.aad5454d.svg"
+  }
+]);
